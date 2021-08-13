e9ea26091d11578585366ab176aea9ef68f9e139
Diego Ernesto <degolego2000@gmail.com>
2021-08-13 01:17:28 -0500

    Quite el plugin para clips

--- vendor/php-clips/src/extension_functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef PHP_CLIPS_EXTENSION_FUNCTIONS
-#define PHP_CLIPS_EXTENSION_FUNCTIONS
-
-#include "utils.h"
-
-void php_call(void* env, DATA_OBJECT_PTR p_return_val);
-void php_method(void* env, DATA_OBJECT_PTR p_return_val);
-
-#endif
